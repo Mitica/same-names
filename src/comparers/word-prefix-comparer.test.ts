e58acdda1ee69532eee856dff8a421ff708538a6
Dumitru Cantea <canteadumitru@gmail.com>
2018-07-10 10:41:36 +0300

    compare words by lowercase

--- src/comparers/word-prefix-comparer.test.ts
+++ src/comparers/word-prefix-comparer.test.ts
@@ -22,11 +22,13 @@ test('same count words', t => {
     const comparer = new WordPrefixComparer();
 
     t.is(comparer.compare('name', 'name'), 1);
-    t.is(comparer.compare('Name one', 'name one'), 0);
+    t.is(comparer.compare('Name one', 'name one'), 1);
     t.is(comparer.compare('name 1', 'name 1'), 1);
     t.true(comparer.compare('Name One', 'Names One') > 0.7);
     t.true(comparer.compare('Vlad Filata', 'Vladimir Filat') > 0.6);
     t.true(comparer.compare('Moldova', 'Moldovei') > 0.7);
     t.true(comparer.compare('Republica Moldova', 'Republicii Moldova') > 0.7);
     t.true(comparer.compare('Respublica Moldova', 'Republica Moldova') === 0);
+    t.true(comparer.compare('REP Moldova', 'Rep Moldova') === 1);
+    t.true(comparer.compare('RIS Moldova', 'RISE Moldova') === 0);
 })
--- src/comparers/word-prefix-comparer.ts
+++ src/comparers/word-prefix-comparer.ts
@@ -17,15 +17,14 @@ export class WordPrefixComparer extends NameComparer {
         for (let i = 0; i < wordsCount; i++) {
             const word1 = name1Words[i];
             const word2 = name2Words[i];
-            if (word1 === word2) {
+            if (word1 === word2 || wordsCount > 1 && word1.toLowerCase() === word2.toLowerCase()) {
                 rating.addWordSameChars(name1Words[i].length, word1, word2, wordsCount, name1, name2);
                 continue;
             }
 
             const sameChars = compareChars(word1, word2);
-            const maxWordLength = Math.max(word1.length, word2.length);
             // !
-            if (sameChars < 2 || sameChars < maxWordLength / 2) {
+            if (!sameWords(sameChars, word1, word2)) {
                 return 0;
             }
             rating.addWordSameChars(sameChars, name1Words[i], name2Words[i], wordsCount, name1, name2);
@@ -45,6 +44,20 @@ function compareChars(word1: string, word2: string): number {
         }
     }
     return matchCount;
+}
+
+function sameWords(same: number, word1: string, word2: string) {
+    if (same < 2 || same < Math.max(word1.length, word2.length) / 2) {
+        return false;
+    }
+    let rests = [word1.substr(same - 1), word2.substr(same - 1)];
+    for (let rest of rests) {
+        if (rest && rest.toLowerCase() !== rest) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 // function startWithUpper(word: string) {
